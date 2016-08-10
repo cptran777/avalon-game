26f6c5a90eacd637bc921b0cf48a4be5d3a4bc5b
cptran777 <cptran14@gmail.com>
2016-08-10 12:30:21 -0700

    progress on game logic and communications

--- server/logic/logic-main.js
+++ server/logic/logic-main.js
@@ -1,15 +1,15 @@
 // Import all functions from modules
-const assignRoles = require('./modules/assignRoles.js');
-const chooseParty = require('./modules/chooseParty.js').chooseParty;
-const resolveParty = require('./modules/chooseParty.js').resolveParty;
-const voteOnParty = require('./modules/voteOnParty.js').voteOnParty;
-const resolvePartyVote = require('./modules/voteOnParty.js').resolvePartyVote;
-const startQuest = require('./modules/decideQuest.js').startQuest;
-const resolveQuest = require('./modules/decideQuest.js').resolveQuest;
-const gameEnd = require('.modules/gameOver.js').gameEnd;
-const gameOver = require('./modules/gameOver.js').gameOver;
-const identifyMerlin = require('./modules/identifyMerlin.js').identifyMerlin;
-const resolveIdMerlin = require('./modules/identifyMerlin.js').resolveIdMerlin;
+var assignRoles = require('./modules/assignRoles.js');
+var chooseParty = require('./modules/chooseParty.js').chooseParty;
+var resolveParty = require('./modules/chooseParty.js').resolveParty;
+var voteOnParty = require('./modules/voteOnParty.js').voteOnParty;
+var resolvePartyVote = require('./modules/voteOnParty.js').resolvePartyVote;
+var startQuest = require('./modules/decideQuest.js').startQuest;
+var resolveQuest = require('./modules/decideQuest.js').resolveQuest;
+var gameEnd = require('.modules/gameOver.js').gameEnd;
+var gameOver = require('./modules/gameOver.js').gameOver;
+var identifyMerlin = require('./modules/identifyMerlin.js').identifyMerlin;
+var resolveIdMerlin = require('./modules/identifyMerlin.js').resolveIdMerlin;
 
 // Export a router to the functions to the game server main file
 // Function expects a type [string] to route to the correct function.
--- server/logic/modules/voteOnParty.js
+++ server/logic/modules/voteOnParty.js
@@ -1,32 +1,53 @@
+var startQuest = 
+
 // Sets up the players to vote on the party chosen by the party leader. 
 module.exports.voteOnParty = function(memcache, socket) {
   // Information needed from memcache
-  // - 
+  // - Current party 
+  var partyMembers = ['<-- FROM MEMCACHE -->'];
 
   // TODO: Set current game phase in memcache to 'VOTE'
 
   // TODO: Signal to players to begin voting. 
-  socket.emit('startVote', {});
+  socket.emit('startVote', {
+    partyMembers
+  });
 
   // TODO: Set timer for resolution to give players time to vote.
+  setTimeout(function() {
+    resolvePartyVote(memcache, socket);
+  }, 500000);
 }
 
-module.exports.resolvePartyVote = function(memcache, socket) {
+var resolvePartyVote = function(memcache, socket) {
   // Information needed from memcache
   // - Player voting results
+  var voteResults = ['<-- FROM MEMCACHE -->'];
   // - Current vetoes count
+  var accepts = '<-- SEARCH VOTE RESULTS FOR ACCEPTS -->';
+  var vetoes = '<-- SEARCH VOTE RESULTS FOR VETOES -->';
   // - Current game phase
+  var gamePhase = '<-- FROM MEMCACHE -->';
 
-  // TODO: If current game phase is not 'VOTE', fizzle
+  // If current game phase is not 'VOTE', fizzle
+  if (gamePhase !== 'VOTE') {
+    return;
+  }
 
   // TODO: Calculate player voting results
   // Accepts > Rejects === Party accepted
   // Rejects >= Accepts === Party rejected
-  if (/* Party accepted */) {
+
+  var partyAccepted = accepts > vetoes ? true : false;
+  if (partyAccepted) {
     // TODO: Signal to players (websockets) that the quest has been
     // accepted
+    socket.emit('resolveVote', {
+      result: 'accepted'
+    });
 
     // TODO: Set timer for startQuest
+    setTimeout
   } else /* Party rejected */ {
     // TODO: Signal to players (websockets) that the quest has been
     // rejected
@@ -44,6 +65,7 @@ module.exports.resolvePartyVote = function(memcache, socket) {
   }
 };
 
+module.exports.resolvePartyVote = resolvePartyVote;
 
 
 
